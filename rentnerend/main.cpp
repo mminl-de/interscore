0cc660c7fcb6ccf6af0bdc9e31a4c8e45dd9534a
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-04-27 23:04:51 +0200

    WindowDisplay layout

--- rentnerend/main.cpp
+++ rentnerend/main.cpp
@@ -27,41 +27,60 @@ extern "C" struct WindowDisplay {
 		QLabel colon;
 	} labels;
 
-WindowDisplay() {
+	WindowDisplay();
+	void update();
+};
+
+extern "C" WindowDisplay::WindowDisplay() {
 	this->widget.setWindowTitle("Interscore: Scoreboard Display");
 
 	// Setting colors
-	this->labels.t1.name.setStyleSheet("color: white;");
-	this->labels.t2.name.setStyleSheet("color: white;");
-	this->labels.t1.score.setStyleSheet("color:" ORANGE ";");
-	this->labels.t2.score.setStyleSheet("color:" ORANGE ";");
-	this->labels.time.setStyleSheet("color: white;");
-	this->labels.colon.setStyleSheet("color: white;");
+	this->widget.setStyleSheet("background-color: black");
+	this->labels.t1.name.setStyleSheet("font-size: 130px; color: white;");
+	this->labels.t2.name.setStyleSheet("font-size: 130px; color: white;");
+	this->labels.t1.score.setStyleSheet("font-size: 600px; color:" ORANGE ";");
+	this->labels.t2.score.setStyleSheet("font-size: 600px; color:" ORANGE ";");
+	this->labels.time.setStyleSheet("font-size: 500px; color: white;");
+	this->labels.colon.setStyleSheet("font-size: 600px; color: white;");
 
-	QHBoxLayout *top_bar = new QHBoxLayout(&this->widget);
+	// Centering everything everywhere
+	this->labels.t1.name.setAlignment(Qt::AlignCenter);
+	this->labels.t2.name.setAlignment(Qt::AlignCenter);
+	this->labels.t1.score.setAlignment(Qt::AlignCenter);
+	this->labels.colon.setAlignment(Qt::AlignCenter);
+	this->labels.t2.score.setAlignment(Qt::AlignCenter);
+	this->labels.time.setAlignment(Qt::AlignCenter);
+
+	// Filling in content
+	this->labels.t1.name.setText("Team 1");
+	this->labels.t1.score.setText("0");
+	this->labels.t2.name.setText("Team 2");
+	this->labels.t2.score.setText("0");
+	this->labels.time.setText("0.00");
+	this->labels.colon.setText(":");
+
+	// Structuring
+	QHBoxLayout *top_bar = new QHBoxLayout();
 	top_bar->addWidget(&this->labels.t1.name);
 	top_bar->addWidget(&this->labels.t2.name);
 
-	QHBoxLayout *middle_bar = new QHBoxLayout(&this->widget);
-	middle_bar->addWidget(&this->labels.t1.score);
-	middle_bar->addWidget(&this->labels.colon);
-	middle_bar->addWidget(&this->labels.t2.score);
+	QHBoxLayout *middle_bar = new QHBoxLayout();
+	middle_bar->addWidget(&this->labels.t1.score, 3);
+	middle_bar->addWidget(&this->labels.colon, 1);
+	middle_bar->addWidget(&this->labels.t2.score, 3);
 
 	QVBoxLayout *layout = new QVBoxLayout(&this->widget);
-	layout->addLayout(top_bar);
-	layout->addLayout(middle_bar);
-	layout->addWidget(&this->labels.time);
+	layout->addLayout(top_bar, 1);
+	layout->addLayout(middle_bar, 2);
+	layout->addWidget(&this->labels.time, 2);
 
-	this->widget.setLayout(layout);
-	// TODO NOW
 	this->update();
 }
 
-void
-update() {
-
+extern "C" void
+WindowDisplay::update() {
+	// TODO
 }
-};
 
 extern "C" struct WindowInput {
 	QWidget widget;
@@ -102,33 +121,35 @@ extern "C" struct WindowInput {
 	} buttons;
 	QComboBox card_dealer;
 
-WindowInput() {
+	WindowInput();
+	void update();
+};
+
+extern "C" WindowInput::WindowInput() {
 	this->widget.setWindowTitle("Interscore: Scoreboard Input");
 	// TODO WIP
 	this->update();
 }
 
-void
-update() {
+extern "C" void
+WindowInput::update() {
 
 }
-};
 
 struct EventFilter : public QObject {
 	WindowDisplay *wd;
 	WindowInput *wi;
 
-EventFilter(WindowDisplay *wd, WindowInput *wi)
-	: wd(wd), wi(wi) {}
+	EventFilter(WindowDisplay *wd, WindowInput *wi) : wd(wd), wi(wi) {}
 
-bool
-eventFilter(QObject *obj, QEvent *event) override {
-	if (event->type() == QEvent::Resize) {
-		wd->update();
-		wi->update();
+	bool
+	eventFilter(QObject *obj, QEvent *event) override {
+		if (event->type() == QEvent::Resize) {
+			wd->update();
+			wi->update();
+		}
+		return QObject::eventFilter(obj, event);
 	}
-	return QObject::eventFilter(obj, event);
-}
 };
 
 QPushButton *
@@ -170,7 +191,7 @@ main(int argc, char *argv[]) {
 	app.installEventFilter(&filter);
 
 	wd.widget.show();
-	wi.widget.show();
+	//wi.widget.show();
 
 	// TODO ADD connect mongoose
 
