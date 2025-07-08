28e18c6bd6d9394c67a16434034f632b3780d791
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-07-08 13:49:01 +0200

    Overwhelmed with common?

---
+++ common/matchday.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "matchday.h"
+
+void matchday_clear(Matchday *md) {
+	if (md.games_count == 0) {
+		printf("There are no games, exiting\n");
+		exit(EXIT_FAILURE);
+	}
+	md.cur.gameindex = 0;
+	md.cur.halftime = 0;
+	md.cur.pause = true;
+	md.cur.time = md.deftime;
+	for (u8 i = 0; i < md.games_count; i++) {
+		md.games[i].halftimescore.t1 = 0;
+		md.games[i].halftimescore.t2 = 0;
+		md.games[i].score.t1 = 0;
+		md.games[i].score.t2 = 0;
+		md.games[i].cards_count = 0;
+		md.games[i].cards = NULL;
+	}
+}
---
+++ common/matchday.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../common.h" // TODO
+
+#pragma pack(push, 1)
+typedef struct {
+	struct {
+		Game *game;
+		bool halftime;
+		bool pause;
+		u16 time;
+		std::chrono::system_clock::time_point start_time;
+	} cur;
+	u16 deftime;
+	Game *games;
+	u8 games_count;
+	Team *teams;
+	u8 teams_count;
+} Matchday;
+#pragma pack(pop)
+
+Matchday matchday_init();
+void matchday_parse_json(Matchday *md, const char *json);
+void matchday_free();
