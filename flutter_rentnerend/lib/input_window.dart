9f6490bea518e491292474b1543425a0657607f9
mrmine <julian@mminl.de>
2025-10-29 14:05:35 +0100

    testing stuff out in dart

--- flutter_rentnerend/lib/input_window.dart
+++ flutter_rentnerend/lib/input_window.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rentnerend/main.dart';
+//import 'package:auto_size_text/auto_size_text.dart';
+//import 'package:flutter_rentnerend/main.dart';
 
 class input_window extends StatelessWidget {
 	const input_window({super.key});
 
 	@override
 	Widget build(BuildContext context) {
+		final screenHeight = MediaQuery.of(context).size.height;
+		final screenWidth = MediaQuery.of(context).size.width;
+
 		return Scaffold(
 			appBar: AppBar(title: const Text('Input Window')),
-			body: Center(
-				child: ElevatedButton(
-					onPressed: () {
-						Navigator.pop(
-							context,
-							MaterialPageRoute<void>(
-								builder: (context) => const LaunchWindow(title: "Home Page (returning)")
+			body: SizedBox(
+				height: screenHeight * 0.15,
+				width: screenWidth,
+				child: Row(
+					children: [
+						Expanded(
+							child: FittedBox(
+								fit: BoxFit.fitWidth,
+								child: Text("NORDSHAUSEN 1")
 							)
-						);
-					},
-					child: const Text('Go back!'),
-				),
-			),
+						),
+						Expanded(
+							child: FittedBox(
+								fit: BoxFit.fitWidth,
+								child: ElevatedButton(onPressed: () {}, child: Text("Switch Side"))
+							)
+						),
+						Expanded(
+							child: FittedBox(
+								fit: BoxFit.fitWidth,
+								child: Text("GIFHORN II")
+							)
+						),
+					]
+				)
+			)
 		);
 	}
 }
