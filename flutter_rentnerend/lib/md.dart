7c5e7a752ca41ed8252868c9b9a0de0805a82d0d
mrmine <julian@mminl.de>
2025-11-17 21:50:36 +0100

    start matchday refactor

---
+++ flutter_rentnerend/lib/md.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:flutter_rentnerend/lib.dart';
+
+class Matchday with _$Matchday {}
+class Meta with _$Meta {}
+class Game with _$Game {}
+class GameTeamSlot with _$GameTeamSlot {}
+class GameQuery with _$GameQuery {}
+class MissingInfo with _$MissingInfo {}
+class Player with _$Player {}
+class Team with _$Team {}
+class Group with _$Group {}
+class GameFormat with _$GameFormat {}
+class GamePart with _$GamePart {}
+class Penalty with _$Penalty {}
+class Shooting with _$Shooting {}
+
