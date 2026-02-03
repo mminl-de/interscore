75f60df72c407bec366b6888a7a61f7c6d9b48dd
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2026-02-03 18:12:20 +0100

    echo: Implemented close behavior

--- echo/src/main.zig
+++ echo/src/main.zig
@@ -2,59 +2,82 @@ const std = @import("std");
 const ws = @import("websocket");
 const AllocatorWrapper = @import("allocator.zig").AllocatorWrapper;
 
-pub fn main() !void {
+const log = std.log.scoped(.interscore);
+const ziglog = std.log.scoped(.zig);
+
+pub fn main() u8 {
+	real_main() catch return 1;
+	return 0;
+}
+
+pub fn real_main() !void {
 	var aw = AllocatorWrapper.init();
 	defer aw.deinit();
 	const gpa = aw.allocator();
 
-	var server = try ws.Server(Handler).init(gpa, .{
-		.port = 9224,
+	var args = std.process.args();
+	_ = args.skip(); // skip "echo"
+
+	var app = App{};
+
+	var server = ws.Server(Handler).init(gpa, .{
+		.port = 8000, // TODO
 		.address = "127.0.0.1",
 		.handshake = .{
 			.timeout = 3,
 			.max_size = 1024,
-			// since we aren't using hanshake.headers
-			// we can set this to 0 to save a few bytes.
 			.max_headers = 0,
 		},
-	});
+	}) catch |err| {
+		log.err("Failed to create WebSocket server!", .{});
+		return err;
+	};
 
-	// Arbitrary (application-specific) data to pass into each handler
-	// Pass void ({}) into listen if you have none
-	var app = App{};
-
-	// this blocks
-	try server.listen(&app);
+	server.listen(&app) catch |err| {
+		log.err("WebSocket server failed to keep listening!", .{});
+		return err;
+	};
 }
 
-// This is your application-specific wrapper around a websocket connection
 const Handler = struct {
 	app: *App,
 	conn: *ws.Conn,
 
-	// You must define a public init function which takes
-	pub fn init(h: *ws.Handshake, conn: *ws.Conn, app: *App) !Handler {
-		// `h` contains the initial websocket "handshake" request
-		// It can be used to apply application-specific logic to verify / allow
-		// the connection (e.g. valid url, query string parameters, or headers)
-
-		_ = h; // we're not using this in our simple case
-
-		return .{
-			.app = app,
-			.conn = conn,
-		};
+	// Callback running after connection establishment has been requested
+	pub fn init(_: *ws.Handshake, conn: *ws.Conn, app: *App) !Handler {
+		log.info("Connecting new client!");
+		return .{ .app = app, .conn = conn };
 	}
 
-	// You must defined a public clientMessage method
+	// Callback running after connection has been established
+	pub fn afterInit(_: *Handler) !void {
+		log.info("New client connected!");
+	}
+
+	// Callback running when client sends message
 	pub fn clientMessage(self: *Handler, data: []const u8) !void {
-		try self.conn.write(data); // echo the message back
+		try self.conn.write(data);
+	}
+
+	// Callback running when connection is about to be terminated
+	pub fn close(self: *Handler) !void {
+		// TODO NOW separate boss and client list
+		if (self.app.bos == self.conn) self.app.boss = null;
+		blk: {
+			for (self.app.clients.items) |*client| {
+				if (client.* == self.conn) {
+					client.* = self.app.clients.items[self.app.clients.items.len - 1];
+					self.app.clients.shrinkRetainingCapacity(self.app.clients.items.len - 1);
+					break :blk;
+				}
+			}
+			unreachable;
+		}
+		log.info("Client disconnected!", .{}); // TODO CONSIDER printing conn id
 	}
 };
 
-// This is application-specific you want passed into your Handler's
-// init function.
 const App = struct {
-	// maybe a db pool
-	// maybe a list of rooms
+	clients: std.ArrayList(?*const ws.Conn),
+	boss: ?*const ws.Conn
 };
